166d999bac6b386057e815cebbee5b03a0cb2811
am1520x <am1520@ic.ac.uk>
2024-10-30 21:48:54 +0000

    Answers UCL-COMP0233-24-25/RSE-Classwork#17 Updated the test function to work with the now yaml format parameterisation.

--- test_times.py
+++ test_times.py
@@ -1,34 +1,20 @@
-from times import compute_overlap_time, time_range
 import pytest
+import yaml
+from times import compute_overlap_time, time_range
 
-"""
-Uses pytest.mark.parametrize to define multiple test cases.
-Each test case includes:
-first_range (first set of time intervals)
-second_range (second set of time intervals)
-expected_overlap (the expected overlapping intervals)
-This is for the 4 test cases:
-1) Overlapping intervals within the same time range
-2) Non-overlapping intervals
-3) Overlapping intervals
-4) Adjacent but non-overlapping intervals.
-"""
-@pytest.mark.parametrize("first_range, second_range, expected_overlap",
-[(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
-  time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60),
-  [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]),
-  (time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00"),
-  time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00", 2, 60),
-  []),
-  (time_range("2010-01-12 10:00:00", "2010-01-12 13:00:00", 3, 900),
-  time_range("2010-01-12 10:40:00", "2010-01-12 11:20:00", 2, 120),
-  [("2010-01-12 10:40:00","2010-01-12 10:50:00"), ("2010-01-12 11:05:00", "2010-01-12 11:20:00")]),
-  (time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00"),
-  time_range("2010-01-12 11:00:00", "2010-01-12 12:45:00"),
-  [])
-])
+# Load the fixture data from the YAML file
+with open('fixtures.yaml', 'r') as file:
+    fixtures = yaml.safe_load(file)
+
+# Prepare the test data
+test_data = []
+for fixture in fixtures:
+    for key, value in fixture.items():
+        time_range_1 = time_range(*value['time_range_1'])
+        time_range_2 = time_range(*value['time_range_2'])
+        expected = [tuple(e) for e in value['expected']]
+        test_data.append((time_range_1, time_range_2, expected))
+
+@pytest.mark.parametrize("first_range, second_range, expected_overlap", test_data)
 def test_time_range_overlap(first_range, second_range, expected_overlap):
-    """
-    Asserts that the output of compute_overlap_time matches the expected_overlap for each set of inputs.
-    """
     assert compute_overlap_time(first_range, second_range) == expected_overlap
